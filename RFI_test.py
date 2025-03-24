3a0083876288a09bdc01e606a3cf03048d04ab04
Richard Rodriguez <rrrodriguez13@berkeley.edu>
2025-03-24 14:47:40 -0700

    reverting RFI_test version because of memory allocation error

--- RFI_test.py
+++ RFI_test.py
@@ -3,24 +3,15 @@ from time import ticks_us, ticks_diff
 
 try:
     analogue_input = ADC(Pin(28))  # ADC pin
-    start_time = ticks_us()
-    duration = 5 * 1_000_000  # 5 seconds in microseconds
-
-    # Buffers for data
-    timestamps = []
-    voltages = []
+    start_time = ticks_us()        # start time in microseconds
+    duration = 5 * 1_000_000       # 5 seconds in microseconds
 
     while ticks_diff(ticks_us(), start_time) < duration:
         current_time = ticks_us()
-        sensor_value = analogue_input.read_u16() / 65535
-        elapsed_time = ticks_diff(current_time, start_time) / 1_000_000
+        sensor_value = analogue_input.read_u16() / 65535  # normalized reading
 
-        timestamps.append(elapsed_time)
-        voltages.append(sensor_value)
-
-    # Print data at once at the end
-    for t, v in zip(timestamps, voltages):
-        print(t, v)
+        elapsed_time = ticks_diff(current_time, start_time) / 1_000_000  # elapsed time in seconds
+        print(elapsed_time, sensor_value)
 
 except KeyboardInterrupt:
     print("Program interrupted. Exiting gracefully.")
